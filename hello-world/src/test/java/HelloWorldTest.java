9e7ff51790a35b136d44558913407d15eb5dfe1d
Ethan Weeks <ethanweeks@ymail.com>
2016-04-16 11:04:18 -0600

    initial commit + solution to hello world

---
+++ hello-world/src/test/java/HelloWorldTest.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelloWorldTest {
+
+    @Test
+    public void helloNoName() {
+        assertEquals("Hello, World!", HelloWorld.hello(""));
+        assertEquals("Hello, World!", HelloWorld.hello(null));
+    }
+
+    @Test
+    public void helloSampleName() {
+        assertEquals("Hello, Alice!", HelloWorld.hello("Alice"));
+    }
+
+    @Test
+    public void helloAnotherSampleName() {
+        assertEquals("Hello, Bob!", HelloWorld.hello("Bob"));
+    }
+
+    @Test
+    public void helloAnotherAnotherSampleName() {
+        assertEquals("Hello, Willy Wonka!", HelloWorld.hello("Willy Wonka"));
+    }
+}
