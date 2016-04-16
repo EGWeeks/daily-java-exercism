5e7a0368332efcf50f9a7ae62f4764a0aaf39046
Ethan Weeks <ethanweeks@ymail.com>
2016-04-16 15:51:20 -0600

    first iteration - passing all!

--- etl/src/main/java/Etl.java
+++ etl/src/main/java/Etl.java
@@ -1,8 +1,25 @@
 import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class Etl {
-   public Map<String, Integer> transform(Map<Integer, List<String>> old) {
-      return null;
-   }
+
+  public Map<String, Integer> transform(Map<Integer, List<String>> old) {
+
+  	Map<String, Integer> letterScore = new HashMap<String, Integer>();
+
+
+  	for(Map.Entry<Integer, List<String>> entry : old.entrySet()) {
+      int value = entry.getKey();
+      List<String> letters = entry.getValue();
+
+      for(int i = 0; i < letters.size(); i++) {
+        letterScore.put(letters.get(i).toLowerCase(), value);
+      }
+  	}
+
+    return letterScore;
+  }
+
 }
