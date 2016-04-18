265c10277ba9459f0f65efdcab8c397c7b5ea120
Ethan Weeks <ethanweeks@ymail.com>
2016-04-17 21:10:49 -0600

    updated nucleotide exercise/setting up basic vars/imports/methods

--- nucleotide-count/src/main/java/DNA.java
+++ nucleotide-count/src/main/java/DNA.java
@@ -1,2 +1,27 @@
+import java.util.map;
+
 public class DNA {
+
+	private String dna;
+
+	public DNA() {
+		dna = dna;
+		Map<String, Integer> nucleotides = new HashMap();
+	}
+
+	// I think I want to do my logic inside of nucleotidecount method
+	// the count method will be for retrieving the value of the dna strand
+	// 
+
+
+	public nucleotideCounts() {
+
+	}
+
+	// This method with check how many time the letter has occured
+	// Even if we have not recieved any DNA Strands
+	public count(String nucleotide) {
+
+	}
+
 }
