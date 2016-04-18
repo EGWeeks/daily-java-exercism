40419465a1d5f4dd6d50f8573a5d62e210ea873f
Ethan Weeks <ethanweeks@ymail.com>
2016-04-17 21:27:55 -0600

    update nucleotide exercise to have create default map when instantiated

--- nucleotide-count/src/main/java/DNA.java
+++ nucleotide-count/src/main/java/DNA.java
@@ -3,25 +3,27 @@ import java.util.map;
 public class DNA {
 
 	private String dna;
+	private Map<String, Integer> nucleotides;
 
 	public DNA() {
 		dna = dna;
-		Map<String, Integer> nucleotides = new HashMap();
+		nucleotides  = new HashMap();
+		nucleotides.put("A", 0);
+		nucleotides.put("C", 0);
+		nucleotides.put("G", 0);
+		nucleotides.put("T", 0);
 	}
 
-	// I think I want to do my logic inside of nucleotidecount method
-	// the count method will be for retrieving the value of the dna strand
-	// 
-
-
+	// not sure if this method needs to return or
+	// if just calling it the test will be able to see the map
+	// might sound stupid but ... i'll figure it out.
 	public nucleotideCounts() {
 
 	}
 
-	// This method with check how many time the letter has occured
-	// Even if we have not recieved any DNA Strands
-	public count(String nucleotide) {
-
+	public int count(String nucleotide) {
+		// This SHOULD return the value of the nucleotide passed in
+		return nucleotides.get(nucleotide);
 	}
 
 }
